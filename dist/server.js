f4f659fccb9809e7628e8ec8aaed608749fb6a09
Alexis Mengual Vázquez <alexismengualvazquez@gmail.com>
2026-08-28 16:52:56 +0200

    fix

--- dist/server.js
+++ dist/server.js
@@ -1,24 +1,21 @@
 import Fastify from "fastify";
 import websocket from "@fastify/websocket";
-
 const fastify = Fastify({ logger: true });
-
 await fastify.register(websocket);
-
 fastify.get("/", { websocket: true }, (socket, req) => {
-    socket.on("message", (message: any) => {
+    socket.on("message", (message) => {
         console.log("Received:", message.toString());
-
         socket.send("hi from server");
     });
 });
-
 try {
     await fastify.listen({
-        port: 3001,
+        port: 3000,
         host: "0.0.0.0",
     });
-} catch (error) {
+}
+catch (error) {
     fastify.log.error(error);
     process.exit(1);
 }
+//# sourceMappingURL=server.js.map
